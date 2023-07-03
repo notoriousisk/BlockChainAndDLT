ae9d7d0c2d35420621134b755766f0f69c3ff66f
Iskander Ishkineev <106443006+notoriousisk@users.noreply.github.com>
2023-07-03 14:56:56 +0300

    Update logo-contract.sol

--- week04/ex01/logo-contract.sol
+++ week04/ex01/logo-contract.sol
@@ -1,1 +1,59 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract TokenMarketplace is ERC721 {
+    uint256 private constant MAX_TOKENS = 1000;
+    uint256 private constant TOKEN_PRICE = 0.001 ether;
+    
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    struct Token {
+        string title;
+        string image;
+    }
+
+    mapping(uint256 => Token) private _tokens;
+    mapping(address => uint256[]) private _userTokens;
+
+    constructor() ERC721("Thousand Logo Page", "TLP") {}
+
+    function buyToken() external payable {
+        require(_tokenIds.current() < MAX_TOKENS, "1000 Logos amount reached");
+        require(msg.value >= TOKEN_PRICE, "Insufficient value sent");
+
+        uint256 newTokenId = _tokenIds.current();
+        _safeMint(msg.sender, newTokenId);
+        _tokens[newTokenId] = Token("ON SALE", "");
+        _tokenIds.increment();
+    }
+
+    function modifyTokenContent(uint256 tokenId, string memory title, string memory image) external {
+        require(_exists(tokenId), "Token does not exist");
+        require(ownerOf(tokenId) == msg.sender, "Not the owner of the token");
+
+        _tokens[tokenId].title = title;
+        _tokens[tokenId].image = image;
+    }
+
+    function transferToken(uint256 tokenId, address to) external {
+        require(_exists(tokenId), "Token does not exist");
+        require(ownerOf(tokenId) == msg.sender, "Not the owner of the token");
+
+        _transfer(msg.sender, to, tokenId);
+    }
+
+    function getUserTokens(address user) external view returns (uint256[] memory) {
+        return _userTokens[user];
+    }
+
+    function getTokenContent(uint256 tokenId) external view returns (string memory title, string memory image) {
+        require(_exists(tokenId), "Token does not exist");
+
+        return (_tokens[tokenId].title, _tokens[tokenId].image);
+    }
+}
 
